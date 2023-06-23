be4d9e4d5fae17750983019b419c7d13c7f8a76b
muhdshanid <shanishanid91@gmail.com>
2023-06-23 10:33:33 +0530

    final commit

--- react-ts-select/src/components/Select.tsx
+++ react-ts-select/src/components/Select.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import styles from '../select.module.css'
 export type SelectOption = {
     label: string,
@@ -22,6 +22,7 @@ type Props = {
 } & (SingleSelectProps | MultipleSelectProps)
 
 const Select = ({ multiple, value, onChange, options}: Props) => {
+    const containerRef = useRef<HTMLDivElement>(null)
     const [highlightedIndex, setHighlightedIndex] = useState(0)
     const [isOpen, setIsOpen] = useState<boolean>(false)
     const clearOptions = () => {
@@ -46,8 +47,42 @@ const Select = ({ multiple, value, onChange, options}: Props) => {
     useEffect(() => {
         if(isOpen) setHighlightedIndex(0)
     },[isOpen])
+    useEffect(() => {
+        const handler = (e: KeyboardEvent) => {
+            if(e.target != containerRef.current) return 
+            switch (e.code) {
+                case "Enter":
+                case "Space":
+                    setIsOpen(prev => !prev)
+                    if(isOpen) selectOption(options[highlightedIndex])
+              break
+              case "ArrowUp":
+              case "ArrowDown":{
+                if(!isOpen){
+                    setIsOpen(true)
+                    break
+                }
+
+                const newValue = highlightedIndex + (e.code === "ArrowDown" ? 1 : -1)
+                if(newValue >= 0 && newValue < options.length){
+                    setHighlightedIndex(newValue)
+                }
+            break
+            }
+            case "Escape":
+                setIsOpen(false)
+            break
+            }
+        }
+            containerRef.current?.addEventListener("keydown", handler)
+
+        return () => {
+            containerRef.current?.removeEventListener("keydown", handler)
+
+        }
+    },[isOpen, highlightedIndex, options])
   return (
-    <div onBlur={() => setIsOpen(false)} onClick={() => setIsOpen(prev => !prev)} className={styles.container}>
+    <div ref={containerRef} onBlur={() => setIsOpen(false)} onClick={() => setIsOpen(prev => !prev)} className={styles.container}>
         <span className={styles.value}>
             {
                 multiple ? 
